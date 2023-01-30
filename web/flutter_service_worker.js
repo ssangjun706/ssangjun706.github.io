c6401b6a29398cec010b48137b34865d35d7619f
ssangjun314 <ssangjun314@outlook.kr>
2023-01-30 14:24:52 +0900

    good

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "80f6a38b85d1ab3361b92a9c8b7e429e",
-"/": "80f6a38b85d1ab3361b92a9c8b7e429e",
-"main.dart.js": "532f35358d56e0026c4b6b35abf8fd2a",
+"index.html": "422c27d5422cfb411ca976f40610c5d1",
+"/": "422c27d5422cfb411ca976f40610c5d1",
+"main.dart.js": "fb5e423471de8e6be57c31fa31d6254c",
 "manifest.json": "230d3e65fcf02255babf4a33cf1a8ef6",
 "version.json": "b5f971bf55581b0f561ad6057ffc11bd"
 };
