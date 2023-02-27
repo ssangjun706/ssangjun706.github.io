5600a09fc919571e25afdb5557f8221d749bd4e5
ssangjun314 <ssangjun314@outlook.kr>
2023-02-28 00:21:38 +0900

    Update

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "da73eb7b014d1f68d280e38e972de874
 "assets/image/pay_sync.png": "6afb51d56b0f160afe45ea3862d56b89",
 "assets/image/salary.png": "654e7f5b5ed41ec612fb171dc74370fc",
 "assets/image/weekly_report.png": "597b986a0f520a32b09a47ef1e42e641",
-"assets/NOTICES": "369879eace1edb6986490cd97af62301",
+"assets/NOTICES": "8da79bc4d86408bfabbc77e136898894",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "57f2f020e63be0dd85efafc7b7b25d80",
 "canvaskit/canvaskit.js": "d8ad2fa33cb436ca011f6077f636fe31",
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "da73eb7b014d1f68d280e38e972de874
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "89c6be6b7243fc89e701fe388f30f6c6",
-"/": "89c6be6b7243fc89e701fe388f30f6c6",
-"main.dart.js": "c46aff4dff683c6e8b9d43c7bdef39a4",
+"index.html": "6aad4e32282224dd62c1975381f87cb1",
+"/": "6aad4e32282224dd62c1975381f87cb1",
+"main.dart.js": "bbd780d21bcb1cd017bc0dbc83e84c97",
 "manifest.json": "3018482620d3b1f627cd73645aac0c2b",
 "version.json": "801cb7283f681e62ba9afe9d3693ae8f"};
 // The application shell files that are downloaded before a service worker can
