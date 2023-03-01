b4713c1721cd6cef94e4a36f5407c860cef40fa6
ssangjun314 <ssangjun314@outlook.kr>
2023-03-02 01:41:09 +0900

    Update

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "59c782f3f504ca44a17847b74e9e1a4a",
 "assets/AssetManifest.json": "77b24977c6c41c77871560c696aa177e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "c0a11c8f8a07918deaa95438fbd2491b",
+"assets/fonts/MaterialIcons-Regular.otf": "c835cc2ed18df18f04150a976b0ddddb",
 "assets/image/account_balance.png": "ce8da815585ca4d946b982df9eabf3f0",
 "assets/image/brand_icon.png": "9ff3744f4f60fc7734f985f60290b989",
 "assets/image/daily_expenditure.png": "4b034826ad438c4228cb6b6a9332e3f4",
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "59c782f3f504ca44a17847b74e9e1a4a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7b22a0a6e0faccda8d9ede1a058b3c18",
-"/": "7b22a0a6e0faccda8d9ede1a058b3c18",
-"main.dart.js": "d7dca40d2705e1d5b2d8b5b5dd941624",
+"index.html": "c22e69653f5bf32cd8e3708dfc7db7de",
+"/": "c22e69653f5bf32cd8e3708dfc7db7de",
+"main.dart.js": "d75575782b72b1480e9dee8b7c7399f2",
 "manifest.json": "3018482620d3b1f627cd73645aac0c2b",
 "version.json": "801cb7283f681e62ba9afe9d3693ae8f"};
 // The application shell files that are downloaded before a service worker can
