5f7f72c42916890295f26b35b9bc9f3892511168
ssangjun314 <ssangjun314@outlook.kr>
2023-03-05 15:27:04 +0900

    Update

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -29,16 +29,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "99dd84a0589aab8287b90766a341fe86
 "assets/shaders/ink_sparkle.frag": "57f2f020e63be0dd85efafc7b7b25d80",
 "canvaskit/canvaskit.js": "d8ad2fa33cb436ca011f6077f636fe31",
 "canvaskit/canvaskit.wasm": "8a69f8da8051cd69aa23d4d5811bb19a",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "4b8f7bd5c3a0fd1b7a6e888cf9999912",
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a2ed22c1907b2a95a4b86249be30709d",
-"/": "a2ed22c1907b2a95a4b86249be30709d",
+"index.html": "1e776919298c080f7b5b26ed3651064e",
+"/": "1e776919298c080f7b5b26ed3651064e",
 "main.dart.js": "acef9609f0e747d66834c4db471e7906",
-"manifest.json": "3018482620d3b1f627cd73645aac0c2b",
+"manifest.json": "9951ed495b937f701154f1857bcfd38c",
 "version.json": "801cb7283f681e62ba9afe9d3693ae8f"};
 // The application shell files that are downloaded before a service worker can
 // start.
